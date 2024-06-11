e2f15d6f310d6bda56b78a89c57b86c296ef73e0
Lucas Maidana <64815326+LMANMAI@users.noreply.github.com>
2024-06-11 10:31:20 -0300

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,22 @@ import * as bodyParser from 'body-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  const allowedOrigins = [
+    'http://localhost:3000',
+    'http://localhost:5173',
+  ];
+
+  app.enableCors({
+    origin: (origin, callback) => {
+      if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+        callback(null, true);
+      } else {
+        callback(new Error('Not allowed by CORS'));
+      }
+    },
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true,
+  });
   app.use(bodyParser.json({ type: 'application/json' }));
 
   await app.listen(process.env.PORT || 4000);
